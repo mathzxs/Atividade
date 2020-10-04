fd7958204f7585d08d46bddb45b9042ec8f33c65
Matheus <matheusgpf@hotmail.com>
2020-10-04 19:27:05 -0300

    Criado

---
+++ src/mapasenhas/Senha.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mapasenhas;
+
+/**
+ *
+ * @author Matheus
+ */
+
+
+public class Senha {
+   protected String tipo;
+
+   protected int numero;
+
+   public String getTipo() {
+
+       return tipo;
+
+   }
+
+   public void setTipo(String tipo) {
+
+       this.tipo = tipo;
+
+   }
+
+   public int getNumero() {
+
+       return numero;
+
+   }
+
+   public void setNumero(int numero) {
+
+       this.numero = numero;
+
+   }
+
+   
+
+}
+
+
---
+++ test/mapasenhas/SenhaIT.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mapasenhas;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Matheus
+ */
+public class SenhaIT {
+    
+    public SenhaIT() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+}
